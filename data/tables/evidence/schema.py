1e465dae4adc4cdb572d2fd1f437530cc3991697
kam557 <kam557@pitt.edu>
2026-07-28 10:31:43 -0400

    Remove duplicate RowData import in schema.py
    Removed duplicate import of RowData in schema.py.

--- data/tables/evidence/schema.py
+++ data/tables/evidence/schema.py
@@ -2,8 +2,6 @@ from pydantic import Field
 from adld_track_hub.utils.models import RowData
 from typing import final
 
-from adld_track_hub.utils.models import RowData
-
 
 @final
 class Schema(RowData):
